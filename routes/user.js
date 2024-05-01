ac1a43726ea1ef2875ba26c621cd7173cc292622
shuQrah <nambejja@iut-dhaka.edu>
2024-05-01 15:48:07 +0600

    This commit involves changes of 'get, update and change password APIs

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+var auth = require('../services/authetication');
+var checkRole = require('../services/checkRole');
 
 router.post('/signup', (req, res) => {
     let user = req.body;
@@ -63,36 +65,36 @@ router.post('/login', (req, res) => {
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth:{
+    auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
     }
 })
 
 // Creating a new API
-router.post('/forgotPassword', (req,res) => {
+router.post('/forgotPassword', (req, res) => {
     const user = req.body;
     query = " select email, password from user where email=?";
     connection.query(query, [user.email], (err, results) => {
-        if(!err) {
-            if(!results[0]) {
-                return res.status(200).json({message: "Password sent successfully to your email."});
+        if (!err) {
+            if (!results[0]) {
+                return res.status(200).json({ message: "Password sent successfully to your email." });
             } else {
                 var mailOptions = {
                     from: process.env.EMAIL,
                     to: results[0].email,
                     subject: 'Password by CafeZestro Management System',
-                    html: '<p><b>Your login details for CafeZestro Management System</b><br><b>Email: </b>'+results[0].email+'<br><b>Password: </b>'+results[0].password+'<br><a href="https://localhost:4200/">Click here to login</a></p>'
+                    html: '<p><b>Your login details for CafeZestro Management System</b><br><b>Email: </b>' + results[0].email + '<br><b>Password: </b>' + results[0].password + '<br><a href="https://localhost:4200/">Click here to login</a></p>'
                     // We will run the frontend from 4200
                 };
-                transporter.sendMail(mailOptions, function(error, info){
-                    if(error) {
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
                         console.log(error);
                     } else {
                         console.log('Email sent:' + info.response);
                     }
                 });
-                return res.status(200).json({message: "Password sent successfully to your email."});
+                return res.status(200).json({ message: "Password sent successfully to your email." });
             }
         } else {
             return res.status(500).json(err);
@@ -101,10 +103,10 @@ router.post('/forgotPassword', (req,res) => {
 })
 
 // API to get all the users
-router.get('/get', (req,res) => {
+router.get('/get', auth.authenticateToken, checkRole.checkRole, (req, res) => {
     var query = "select id, name, email, contactNumber, status from user where role = 'user'"
     connection.query(query, (err, results) => {
-        if(!err) {
+        if (!err) {
             return res.status(200).json(results);
         } else {
             return res.status(500).json(err);
@@ -113,15 +115,15 @@ router.get('/get', (req,res) => {
 })
 
 // API to change the status of user from the UI
-router.patch('/update', (req,res) => {
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res) => {
     let user = req.body;
     var query = "update user set status=? where id=?";
     connection.query(query, [user.status, user.id], (err, results) => {
-        if(!err) {
-            if(results.affectedRows == 0) {
-                return res.status(404).json({message: "User ID does not exist"});
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "User ID does not exist" });
             }
-            return res.status(200).json({message: "User Updated Successfully"});
+            return res.status(200).json({ message: "User Updated Successfully" });
         } else {
             return res.status(500).json(err);
         }
@@ -129,14 +131,38 @@ router.patch('/update', (req,res) => {
 })
 
 // API to check token
-router.get('/checkToken', (req,res) => {
-    return res.status(200).json({message: "true"});
+router.get('/checkToken', auth.authenticateToken, (req, res) => {
+    return res.status(200).json({ message: "true" });
 })
 
 // API to change the password
-router.post('/changePassword', (req,res) => {
-    //const 
-    
+router.post('/changePassword', auth.authenticateToken, (req, res) => {
+    const user = req.body;
+    const email = res.locals.email;
+    var query = "select *from user where email=? and password=?";
+    connection.query(query, [email, user.oldPassword], (err, results) => {
+        if (!err) {
+            if (!results[0]) {
+                return res.status(400).json({ message: "Incorrect Old Password" });
+            }
+            else if (results[0].password == user.oldPassword) {
+                query = "update user set password=? where email=?";
+                connection.query(query, [user.newPassword, email], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Password Updated Successfully" });
+                    } else {
+                        return res.status(500).json(err);
+                    }
+                })
+            } else {
+                return res.status(400).json({ message: "Something went wrong.Please try again later" });
+            }
+
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+
 })
 
 module.exports = router;
--- services/authetication.js
+++ services/authetication.js
@@ -3,13 +3,17 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split[' '][1]
-    if (token == null)
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) {
+        // Unauthorized
         return res.sendStatus(401);
+    }
 
+    // If Authorized Not Signed
     jwt.verify(token, process.env.ACCESS_TOKEN, (err, response) => {
-        if (err)
+        if (err) {
             return res.sendStatus(403);
+        }
         res.locals = response;
         next()
     })

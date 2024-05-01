6d3628c5d0f18e1456990b388434a5d32df95e43
akilahjahin <akilahjahin@iut-dhaka.edu>
2024-05-01 23:46:09 +0600

    The changes made to include all APIs of Bill; The html code for the generation of the BillingOrderInfo for printout

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
-const userRoute = require ('./routes/user');
+const userRoute = require('./routes/user');
 const categoryRoute = require('./routes/category');
 const productRoute = require('./routes/product');
+const billRoute = require('./routes/bill');
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', userRoute); // Whenever any API is hit, /user will go to userRoute and then to other parts
-app.use('/category',categoryRoute);
-app.use('/product',productRoute);
+app.use('/category', categoryRoute);
+app.use('/product', productRoute);
+app.use('/bill', billRoute);
 
 module.exports = app;
